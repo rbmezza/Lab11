2435c09c4c61e99fdca09f0c2053ee2c470dde91
rbocanegramez <rbmezza@gmail.com>
2016-11-24 16:28:05 -0500

    notification added

--- app/src/main/java/com/example/rbocanegramez/myapplication/MyService.java
+++ app/src/main/java/com/example/rbocanegramez/myapplication/MyService.java
@@ -1,15 +1,23 @@
 package com.example.rbocanegramez.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class MyService extends Service {
+
+    int notificationCount;
+
     /*
     AVOID CALLING ANY ANDROID APIS FROM HERE ex. Context.whatever.
      */
     public MyService() {
+        notificationCount = 1;
     }
 
     /*
@@ -23,7 +31,29 @@ public class MyService extends Service {
         //TODO: use alarm manager for notification for the lab.
         //for now we are just going to display a toast
 
-        Toast.makeText(this,"service starting", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this,"service starting", Toast.LENGTH_LONG).show();
+
+        //Adding a notification:
+        //Show notification when intent starts
+        Notification.Builder mBuilder = new Notification.Builder(this).setSmallIcon(null).
+                setContentTitle("Service was started").setContentText("This is the body text");
+
+        //create a implicit Intent that will be broadcast when the user clicks pm the Notification
+        Intent notificationIntent = new Intent("com.example.rbocanegramez.NOTIFICATION_CLICKED");
+
+        //create pending PendingIntent to package the above intent
+        PendingIntent notificationPendingIntent = PendingIntent.getBroadcast(this,123,notificationIntent,0);
+        //associate the two using our Notification.Builder instance:
+        mBuilder.setContentIntent(notificationPendingIntent);
+
+        //get an instance of notification manager:
+        //for location it is the same but with location service.
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Notify the user (fire off the notification)
+        //for notifications to not overlap the notification id needs to be different.
+        //notificationManager.notify(1,mBuilder.build());
+        notificationManager.notify(notificationCount++,mBuilder.build());
 
         return super.onStartCommand(intent, flags, startId);
     }
